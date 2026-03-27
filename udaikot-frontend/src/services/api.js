7c6e1465c7f23285d3f49dd0fdccb0bacaa44a55
Monil <sethmonil30@gmail.com>
2026-03-27 21:30:22 +0530

    fixed api route

--- udaikot-frontend/src/services/api.js
+++ udaikot-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://udaikot-backend.onrender.com",
+  baseURL: "https://udaikot-backend.onrender.com/api",
 });
 
 export default API;
